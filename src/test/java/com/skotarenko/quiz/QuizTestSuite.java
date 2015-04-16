aca9240890fb9f4c0243a5a4abb41580cbe4b8e4
maxyms <ms.addt@gmail.com>
2015-04-16 16:47:47 -0400

    Add more solutions: - ExcelSheetColumnNumber - ExcelSheetColumnTitle - PlusOne - RemoveArrayElement - RemoveDuplicatesFromSortedArray

--- src/test/java/com/skotarenko/quiz/QuizTestSuite.java
+++ src/test/java/com/skotarenko/quiz/QuizTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ BinaryTreeTest.class, CountAndSayTest.class, GasStationTest.class, LongestValidParenthesesTest.class, NumberOfBitsTest.class, PascalTriangleTest.class,
-        PascalTriangle2Test.class, ReverseBitsTest.class, ValidParenthesesTest.class, MajorityElementTest.class, RotateArrayTest.class })
+        PascalTriangle2Test.class, ReverseBitsTest.class, ValidParenthesesTest.class, MajorityElementTest.class, RotateArrayTest.class, ExcelSheetColumNumberTest.class,
+        ExcelSheetColumTitleTest.class, RemoveArrayElementTest.class, RemoveDuplicatesFromSortedArrayTest.class, PlusOneTest.class })
 public class QuizTestSuite {
 }
