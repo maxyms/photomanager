0b2d46debdd9ee586f8a4c21602c147ba5e3eabe
maxyms <ms.addt@gmail.com>
2015-05-26 18:13:50 -0400

    Adding quizes: ContainsDuplicate RemoveNthNodeFromEndOfList SameTree TwoSum

--- src/main/java/com/skotarenko/quiz/RotateArray.java
+++ src/main/java/com/skotarenko/quiz/RotateArray.java
@@ -1,5 +1,7 @@
 package com.skotarenko.quiz;
 
+import java.util.Arrays;
+
 public class RotateArray {
     public void rotate1(int[] nums, int k) {
         final int l = nums.length;
@@ -33,41 +35,75 @@ public class RotateArray {
     }
 
     public void rotate3(int[] nums, int k) {
-        final int l = nums.length;
-        k = k % l;
-        int index = l - 1;
-        int elem = nums[index];
-        int tmp = 0;
-        int newInd = 0;
-        for (int i = 0; i < l; i++) {
-            newInd = index + k;
-            if (newInd >= l) {
-                newInd -= l;
+        int tmp = 0, p = 0, l = nums.length;
+        if ((l == 0) || (k = k % l) == 0) {
+            return;
+        }
+        System.out.println(Arrays.toString(nums));
+        while (p < l - k) {
+            System.out.println("Next iteration");
+            for (int i = p; i < p + k; i++) {
+                tmp = nums[i];
+                nums[i] = nums[l - k + (i - p)];
+                nums[l - k + (i - p)] = tmp;
+                System.out.println(Arrays.toString(nums));
             }
-            tmp = nums[newInd];
-            nums[newInd] = elem;
-            elem = tmp;
-            index = newInd;
+            p += k;
+        }
+        int newK = 2 * k - l;
+        int newL = 2 * l - 3 * k;
+        p = l - k;
+        k = newK;
+        l = newL;
+        while (p < l - k) {
+            System.out.println("Next iteration");
+            for (int i = p; i < p + k; i++) {
+                tmp = nums[i];
+                nums[i] = nums[l - k + (i - p)];
+                nums[l - k + (i - p)] = tmp;
+                System.out.println(Arrays.toString(nums));
+            }
+            p += k;
         }
     }
 
     public void rotate4(int[] nums, int k) {
-        final int l = nums.length;
-        k = k % l;
-        int index = l - 1;
-        int elem = nums[index];
-        int tmp = 0;
-        int newInd = 0;
-        for (int i = 0; i < l; i++) {
-            newInd = index + k;
-            if (newInd >= l) {
-                newInd -= l;
-            }
-            tmp = nums[newInd];
-            nums[newInd] = elem;
-            elem = tmp;
-            index = newInd;
+        int tmp = 0, p = 0, l = nums.length;
+        if ((l == 0) || (k = k % l) == 0) {
+            return;
         }
+        if (l % k == 0) {
+            for (int j = 0; j < k; j++) {
+                int index = l - 1;
+                int elem = nums[index];
+                int newInd = j;
+                for (int i = 0; i < l / k; i++) {
+                    newInd = index + k;
+                    if (newInd >= l) {
+                        newInd -= l;
+                    }
+                    tmp = nums[newInd];
+                    nums[newInd] = elem;
+                    elem = tmp;
+                    index = newInd;
+                }
+            }
+        } else {
+            int index = l - 1;
+            int elem = nums[index];
+            int newInd = 0;
+            for (int i = 0; i < l; i++) {
+                newInd = index + k;
+                if (newInd >= l) {
+                    newInd -= l;
+                }
+                tmp = nums[newInd];
+                nums[newInd] = elem;
+                elem = tmp;
+                index = newInd;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
     }
 
     public void rotate(Object[] nums, int k) {
