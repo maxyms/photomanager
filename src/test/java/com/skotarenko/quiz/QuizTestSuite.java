3a8dcbeb7c126e7804ed0c9e8391cd9026a05a5c
maxyms <ms.addt@gmail.com>
2015-06-01 11:21:08 -0400

    Add more tests: IsomorphicStrings RemoveLinkedListElements SymmetricTree

--- src/test/java/com/skotarenko/quiz/QuizTestSuite.java
+++ src/test/java/com/skotarenko/quiz/QuizTestSuite.java
@@ -3,10 +3,16 @@ package com.skotarenko.quiz;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * https://leetcode.com
+ * 
+ * @author maxyms
+ *
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ BinaryTreeTest.class, CountAndSayTest.class, GasStationTest.class, LongestValidParenthesesTest.class, NumberOfBitsTest.class, PascalTriangleTest.class,
         PascalTriangle2Test.class, ReverseBitsTest.class, ValidParenthesesTest.class, MajorityElementTest.class, RotateArrayTest.class, ExcelSheetColumNumberTest.class,
         ExcelSheetColumTitleTest.class, RemoveArrayElementTest.class, RemoveDuplicatesFromSortedArrayTest.class, PlusOneTest.class, MergeSortedArrayTest.class,
-        ContainsDuplicateTest.class, SameTreeTest.class, RemoveNthNodeFromEndOfListTest.class })
+        ContainsDuplicateTest.class, SameTreeTest.class, RemoveNthNodeFromEndOfListTest.class, SymmetricTreeTest.class, IsomorphicStringsTest.class, RemoveLinkedListElements.class })
 public class QuizTestSuite {
 }
